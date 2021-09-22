bbda5260d6a906e07bb22f47b6de0f7d7f301e2d
swetank13 <swetank93anand@gmail.com>
2021-09-22 23:23:16 +0530

    Added configuration file to repo

---
+++ src/main/scala/com/swetank/config/SparkConfig.scala
@@ -0,0 +1,14 @@
+package com.swetank.config
+
+import org.apache.spark.sql.SparkSession
+
+object SparkConfig {
+  def getSparkSession: SparkSession = {
+    SparkSession.builder()
+      .config("spark.network.timeout", "800")
+      .config("spark.io.compression.codec", "snappy")
+      .config("spark.serializer", "org.apache.spark.serializer.kryoSerializer")
+      .getOrCreate()
+  }
+
+}
