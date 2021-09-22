52dbadf2e8ec129c9b9d6b6d8a47bf4084c5f610
swetank13 <swetank93anand@gmail.com>
2021-09-22 23:30:34 +0530

    Change in import for constant class

--- src/main/scala/com/swetank/constants/Constants.scala
+++ src/main/scala/com/swetank/constants/Constants.scala
@@ -1,7 +1,7 @@
 package com.swetank.constants
 
+import com.swetank.config.SparkConfig
 import org.apache.spark.sql.SparkSession
-import com.swetank.utility.SparkConfig
 
 object Constants {
   val spark: SparkSession = SparkConfig.getSparkSession
